7a81425764934cb81e00996b4638f72b77a79141
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-06-12 10:37:35 +0545

    goodreads update

--- test.py
+++ test.py
@@ -16,30 +16,43 @@ username = browser.find_element_by_name("identifier")
 # browser.find_element_by_class_name('whsOnd')
 username.send_keys('saral.karki@gmail.com')
 username.send_keys(Keys.ENTER)
-
-try:
-    element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.NAME, "password"))
+# since the script runs fast it does not get enough time to find the item. Hence we add a wait method to 
+#wait until it finds the locator
+# try:
+#     element = WebDriverWait(browser, 10).until(
+#         EC.presence_of_element_located((By.NAME, "password"))
         
-    )
-finally:
-    pass
+#     )
+# finally:
+#     pass
+browser.implicitly_wait(10)
+element = browser.find_element_by_name('password')
 element.send_keys('liv3rpool fc1892')
 element.send_keys(Keys.ENTER)
+# book read update
+# currently_reading = browser.find_element_by_xpath('//*[@href="/book/show/31706504-barking-up-the-wrong-tree"]')
+# print(currently_reading)
+
+# browser.implicitly_wait(10)
+# update the current book
+# book_up  = browser.find_element_by_class_name("gr-book__additionalContent")
+update = browser.find_element(By.XPATH, '//button[text()="Update progress"]')
+update.click()
+browser.implicitly_wait(10)
+
+#the popup div
+progress = browser.find_element_by_class_name("updateReadingProgress__headerInput")
+progress.clear()
+progress.send_keys('181')
+
+# click Update progress link
+final_update = browser.find_element(By.XPATH, '//button[text()="Update progress"]')
+final_update.click()
+browser.implicitly_wait(5)
+
+browser.quit()
 
 
 
-
-# browser.quit()
-
-# browser.find_element_by_xpath('//*[@name="password"]')
-# pwd = browser.find_element_by_name('password')
-
-# pwd  = browser.find_element_by_xpath('//*[@aria-label="तपाईको पासवर्ड प्रविष्टि गर्नुहोस्"]')
-# # pwd = browser.find_element_by_name("password")
-# print(pwd.text)
-# pwd.send_keys('liv3rpool fc1892')
-# pwd.send_keys(Keys.ENTER)
-# #hit enter
 
 
