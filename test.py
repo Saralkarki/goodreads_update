345bd4f242467d712745f4a86717c008a9a66376
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-06-12 08:56:19 +0545

    got the password working

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 #import the chrome driver for opening the browser
 browser = webdriver.Chrome('/users/saral/Desktop/chromedriver')
@@ -9,18 +12,34 @@ browser.get('http://www.goodreads.com/')
 #sign in with google
 browser.find_element_by_xpath('//*[@title="Sign in with your Google account"]').click()
 #fill in the google credentials
-
-username = browser.find_element_by_class_name('whsOnd')
+username = browser.find_element_by_name("identifier")
+# browser.find_element_by_class_name('whsOnd')
 username.send_keys('saral.karki@gmail.com')
 username.send_keys(Keys.ENTER)
 
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.NAME, "password"))
+        
+    )
+finally:
+    pass
+element.send_keys('liv3rpool fc1892')
+element.send_keys(Keys.ENTER)
+
+
+
+
+# browser.quit()
 
 # browser.find_element_by_xpath('//*[@name="password"]')
 # pwd = browser.find_element_by_name('password')
-element = browser.find_element_by_xpath('//*[@type="password"]')
-print(element.text)
-element.send_keys('liv3rpool fc1892')
-element.send_keys(Keys.ENTER)
-#hit enter
+
+# pwd  = browser.find_element_by_xpath('//*[@aria-label="तपाईको पासवर्ड प्रविष्टि गर्नुहोस्"]')
+# # pwd = browser.find_element_by_name("password")
+# print(pwd.text)
+# pwd.send_keys('liv3rpool fc1892')
+# pwd.send_keys(Keys.ENTER)
+# #hit enter
 
 
